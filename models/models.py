fa8c6d7ecbd9224ea7550d8af254c558e9b6a13c
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-02-22 12:34:33 +0200

    Add models for steps and weather

---
+++ models/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Steps(Base):
+    __tablename__ = "steps"
+
+    datauuid = Column(String, primary_key=True)
+    binning_data = Column(String)
+    update_time = Column(String)
+    create_time = Column(String)
+    source_pkg_name = Column(String)
+    source_type = Column(Integer)
+    count = Column(Integer)
+    speed = Column(Float)
+    distance = Column(Float)
+    calorie = Column(Float)
+    deviceuuid = Column(String)
+    pkg_name = Column(String)
+    day_time = Column(Integer)
+
+
+class Weather(Base):
+    __tablename__ = "weather"
+
+    time = Column(String, primary_key=True)
+    tavg = Column(Float)
+    tmin = Column(Float)
+    tmax = Column(Float)
+    prcp = Column(Float)
+    snow = Column(Float)
+    wdir = Column(Float)
+    wspd = Column(Float)
+    wpgt = Column(Float)
+    pres = Column(Float)
+    tsun = Column(Float)
