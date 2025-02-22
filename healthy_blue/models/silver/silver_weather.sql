b36ab7f0f0eb0b10b9dd5c6e91ccf0c8ea3f01f2
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-02-22 14:40:47 +0200

    Add silver table being renaming

---
+++ healthy_blue/models/silver/silver_weather.sql
@@ -0,0 +1,14 @@
+SELECT
+    time as recorded_date,
+    tavg as average_temperature,
+    tmin as minimum_temperature,
+    tmax as maximum_temperature,
+    prcp as precipitation,
+    snow,
+    wdir as wind_direction,
+    wspd as wind_speed,
+    wpgt as wind_gust_speed,
+    pres as presure,
+    tsun as sunshine_duration
+FROM 
+    {{ref('bronze_weather')}}
