9a552399289daf8f3a2d3d40f915053482ed4f7d
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-03-02 23:47:23 +0200

    Remove unused methods - will add if needed later

--- db/database.py
+++ db/database.py
@@ -9,17 +9,8 @@ class Database:
         self.db_url = db_url
         self.engine = create_engine(self.db_url)
         self.Session = sessionmaker(bind=self.engine)
-
-    def create_tables(self):
-        """Creates tables in the database from the Base metadata"""
         Base.metadata.create_all(self.engine)
-        print("Tables created successfully.")
 
     def get_session(self):
-        """Returns a session object to interact with the database"""
         return self.Session()
 
-    def drop_tables(self):
-        """Drops all tables from the database"""
-        Base.metadata.drop_all(self.engine)
-        print("Tables dropped successfully.")
