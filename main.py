4cdbbe979b9256e0273c83a435cc22b165b5fdd4
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-03-02 23:46:31 +0200

    Update main file to run ingestion

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from ingest import steps, weather
+from db.database import Database
+from ingest.ingest import Ingest
+from sorting_data import sorting_data
+data_directory = "data/"
+
+sorting_data(data_directory=data_directory)
+
+db_path = "data/health.db"
+database = Database(db_url=f"sqlite:///{db_path}")
+ingest = Ingest(data_directory=data_directory, database=database)
+
+ingest.ingest_steps(steps_file_name="step_daily_trend.csv")
+ingest.ingest_weather()
