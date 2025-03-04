9fb4f4396201b1383f3a808599790b84f5a0e446
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-03-04 09:52:12 +0200

    Remove distinct

--- healthy_blue/models/silver/silver_steps.sql
+++ healthy_blue/models/silver/silver_steps.sql
@@ -1,5 +1,5 @@
 -- Would prefer agg instead of distinct, could be losing data: SUM(count), where clause, to get certain soure type: Need to understand what it is. Might be supporting data, take AVG in that case. Make informed dec if you can't find out
-SELECT DISTINCT
+SELECT
     DATE(datetime(day_time / 1000, 'unixepoch')) AS recorded_date, 
     "count", 
     speed, 
