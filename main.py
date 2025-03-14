68ecbcbcca28d64f8c207b3721a3b667c9a7f44a
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-03-14 14:32:52 +0200

    Add streamlit and compile and run of dbt

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
+import streamlit as st
+import subprocess
+
 from db.database import Database
 from ingest.ingest import Ingest
+from models.city import City
 from sorting_data import sorting_data
+from util import compile_run_dbt
 
 data_directory = "data"
 raw_data_directory = "data/raw"
 clean_data_directory = "data/clean"
-# This assumes a folder is uploaded
-uploaded_folder = "samsunghealth_jasonlsmith14_20250218135116"
 
-cleaned_data_path = sorting_data(
-    raw_data_directory=raw_data_directory,
-    cleaned_data_directory=clean_data_directory,
-    uploaded_folder=uploaded_folder,
+cities = [
+    City(-26.2041, 28.0473, 1753, "Johannesburg"),
+    City(-29.8587, 31.0218, 8, "Durban"),
+    City(-33.9249, 18.4241, 42, "Cape Town"),
+]
+
+files = st.file_uploader(
+    label="Upload your health data files", accept_multiple_files=True
 )
 
-db_path = f"{data_directory}/health.db"
-database = Database(db_url=f"sqlite:///{db_path}")
-ingest = Ingest(data_directory=clean_data_directory, database=database)
-
-ingest.ingest_steps(
-    folder_path=cleaned_data_path, steps_file_name="step_daily_trend.csv"
+option = st.selectbox(
+    "What was your primary location?",
+    (city.location_name for city in cities),
 )
-ingest.ingest_weather()
+
+if st.button(label="Upload files") and option:
+    if not files:
+        raise ValueError("Files were not uploaded")
+
+    city = next((city for city in cities if city.location_name == option), None)
+
+    for file in files:
+        with open(f"{raw_data_directory}/{file.name}", "wb") as f:
+            bytes_data = file.read()
+            f.write(bytes_data)
+
+    uploaded_folder = ""
+
+    cleaned_data_path = sorting_data(
+        raw_data_directory=raw_data_directory,
+        cleaned_data_directory=clean_data_directory,
+        uploaded_folder=uploaded_folder,
+    )
+
+    db_path = f"{data_directory}/health.db"
+    database = Database(db_url=f"sqlite:///{db_path}")
+    ingest = Ingest(data_directory=clean_data_directory, database=database, city=city)
+
+    ingest.ingest_steps(
+        folder_path=cleaned_data_path, steps_file_name="step_daily_trend.csv"
+    )
+    ingest.ingest_weather()
+
+    compile_command = ["dbt", "compile"]
+    run_command = ["dbt", "run"]
+
+    compile_run_dbt(compile_command=compile_command, run_command=run_command)
