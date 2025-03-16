fab2cbb2e6f1649aa3e3f63eda144d08d9e8c01a
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-03-16 22:31:07 +0200

    Handle states for read in data Add insights

--- pages/insights_page.py
+++ pages/insights_page.py
@@ -3,10 +3,28 @@ from config import database
 import streamlit as st
 
 from util import human_readable
+st.set_page_config(layout="wide")
 
-gold_steps_by_year = pd.read_sql("SELECT * FROM gold_steps_by_year", database.engine)
-gold_steps_by_month = pd.read_sql("SELECT * FROM gold_steps_by_month", database.engine)
-gold_steps_by_day = pd.read_sql("SELECT * FROM gold_steps_by_day", database.engine)
+if "gold_steps_by_year" not in st.session_state:
+    gold_steps_by_year = pd.read_sql(
+        "SELECT * FROM gold_steps_by_year", database.engine
+    )
+    st.session_state["gold_steps_by_year"] = gold_steps_by_year
+
+if "gold_steps_by_month" not in st.session_state:
+    gold_steps_by_month = pd.read_sql(
+        "SELECT * FROM gold_steps_by_month", database.engine
+    )
+    st.session_state["gold_steps_by_month"] = gold_steps_by_month
+
+if "gold_steps_by_day" not in st.session_state:
+    gold_steps_by_day = pd.read_sql("SELECT * FROM gold_steps_by_day", database.engine)
+    st.session_state["gold_steps_by_day"] = gold_steps_by_day
+
+
+gold_steps_by_year: pd.DataFrame = st.session_state["gold_steps_by_year"]
+gold_steps_by_month: pd.DataFrame = st.session_state["gold_steps_by_month"]
+gold_steps_by_day: pd.DataFrame = st.session_state["gold_steps_by_day"]
 
 years = gold_steps_by_year["year"]
 year_option = st.selectbox(
@@ -15,8 +33,12 @@ year_option = st.selectbox(
     options=(year for year in years),
 )
 
-day_with_most_steps_tab, month_with_most_steps_tab = st.tabs(
-    ["Day with most Steps", "Month with most Steps"]
+day_with_most_steps_tab, month_with_most_steps_tab, steps_over_the_year = st.tabs(
+    [
+        "Your day with most steps",
+        "Your month with most steps",
+        f"Your steps over the year",
+    ]
 )
 
 with day_with_most_steps_tab:
@@ -31,10 +53,46 @@ with day_with_most_steps_tab:
 
 with month_with_most_steps_tab:
     data = gold_steps_by_month.copy()
-    data["month_of_the_year"] = pd.to_datetime(data["month_of_the_year"], format="%Y-%m")
+    data["month_of_the_year"] = pd.to_datetime(
+        data["month_of_the_year"], format="%Y-%m"
+    )
     data = data[data["month_of_the_year"].dt.year == int(year_option)]
     row = data.loc[data["total_steps"].idxmax()]
 
     st.write(
         f"You took the most steps in {row["month_of_the_year"].strftime("%B")} with a total of {human_readable(row["total_steps"])} steps"
     )
+
+
+with steps_over_the_year:
+    data = gold_steps_by_month.copy()
+    data["month_of_the_year"] = pd.to_datetime(
+        data["month_of_the_year"], format="%Y-%m"
+    )
+    data = data[data["month_of_the_year"].dt.year == int(year_option)]
+    data["month_of_the_year"] = data["month_of_the_year"].apply(
+        lambda x: x.strftime("%B")
+    )
+
+    month_order = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+
+    # Convert "month" column to categorical with custom order
+    data["month_of_the_year"] = pd.Categorical(data["month_of_the_year"], categories=month_order, ordered=True)
+
+    # Sort the data according to the defined order
+    data = data.sort_values("month_of_the_year")
+
+    st.bar_chart(data=data, x="month_of_the_year", y="total_steps", x_label="Month", y_label="Steps")
